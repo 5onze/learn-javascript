de8ad76daec821483d1c9a03037b3c82e63c1f06
5onze <darkerhorn@gmail.com>
2022-06-10 03:52:23 +0900

    add Getter and setters, Inheritance, instanceOf

--- notes/class.js
+++ notes/class.js
@@ -25,3 +25,107 @@ const ellie = new Person('ellie', 20);
 console.log(ellie.name);
 console.log(ellie.age);
 ellie.speak();
+
+
+// 2. Getter and setters
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        // if (value < 0) {
+        //     throw Error('age can not be negative');
+        // }
+        this._age = value < 0 ? 0 : value;
+    }
+}
+
+const user1 = new User('Steve', 'job', -1);
+console.log(user1.age);
+
+
+// 3. Fields (public, private)
+// Too soon!
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+class Experiment {
+    publicField = 2;
+    #privateField = 0;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
+
+
+// 5. Inheritance (상속과 다양성)
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color!`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {} // Shape이 Rectangle에 연장됨(포함됨)
+class Triangle extends Shape { // 필요한 부분만 재정의 할수있다. 오버라이딩
+    draw() {
+        super.draw(); // 부모의 메소드 호출
+        console.log('🔺');
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+
+    toString() {
+        return `Triangle: color: ${this.color}`;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle); //true
+console.log(triangle instanceof Rectangle); // false
+console.log(triangle instanceof Triangle); //true
+console.log(triangle instanceof Shape); //true
+console.log(triangle instanceof Object); //true
+console.log(triangle.toString());
